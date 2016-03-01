461ea0511bdcdf457517a67d90722405848383a5
Thierry BM <thierry.bazier-matte@hec.ca>
2016-03-01 16:32:21 -0500

    hello

--- bug.py
+++ bug.py
@@ -1,20 +1,25 @@
+#!/usr/local/bin/python3
+
 import cvxpy as cvx
 import numpy as np
 
 from lipschitzexp import LipschitzExp
 
 
-p = 3
-n = 10000
+r = np.array([1,1,1])
+x = -r
+
+n = len(r)
+p = 1
 
 u = lambda r: LipschitzExp(r,1,0)
 q = cvx.Variable(p)
 
-r = np.random.normal(8,10,size=n)
-x = np.random.multivariate_normal(np.zeros(p),np.eye(p),size=n)
 
-obj = cvx.Minimize(-1/n * cvx.sum_entries(u( cvx.mul_elemwise(r,x*q)  )) + 1500*cvx.norm(q)**2)
+obj = cvx.Minimize(-1/n * cvx.sum_entries(u( cvx.mul_elemwise(r,x*q)  )) + 10*cvx.norm(q)**2)
 pr = cvx.Problem(obj)
+print('r=',r)
+print('x=',x)
 val = pr.solve()
 
 print(val)
--- lipschitzexp.py
+++ lipschitzexp.py
@@ -8,19 +8,19 @@ from cvxpy.atoms.elementwise.exp import exp
 
 
 class LipschitzExp(Elementwise):
-    def __init__(self,x,β=1,x0=0):
-        self.β = self.cast_to_const(β)
+    def __init__(self,x,beta=1,x0=0):
+        self.beta = self.cast_to_const(beta)
         self.x0 = self.cast_to_const(x0)
         super().__init__(x)
 
     @Elementwise.numpy_numeric
     def numeric(self,values):
-        β = self.β.value
+        beta = self.beta.value
         x0 = self.x0.value
         x = values[0]
         exp = np.exp
-        return (x >= x0)* (1/β * (1 - np.exp(-β*x))) + \
-               (x < x0) * (x*np.exp(-β*x0) + 1/β*(1-(1+β*x0)*np.exp(-β*x0)))
+        return (x >= x0)* (1/beta * (1 - np.exp(-beta*x))) + \
+               (x < x0) * (x*np.exp(-beta*x0) + 1/beta*(1-(1+beta*x0)*np.exp(-beta*x0)))
 
     def sign_from_args(self):
         return u.Sign.UNKNOWN
@@ -32,53 +32,54 @@ class LipschitzExp(Elementwise):
         return [u.monotonicity.DECREASING]
 
     def get_data(self):
-        return [self.β,self.x0]
+        return [self.beta,self.x0]
 
     def validate_arguments(self):
-        '''Check that β>0.'''
-        if not (self.β.is_positive() and
-                self.β.is_constant() and self.β.is_scalar() and
+        '''Check that beta>0.'''
+        if not (self.beta.is_positive() and
+                self.beta.is_constant() and self.beta.is_scalar() and
                 self.x0.is_constant() and self.x0.is_scalar()):
-            raise ValueError('β must be a non-negative scalar constant, and x0 must a constant scalar.')
+            raise ValueError('beta must be a non-negative scalar constant, and x0 must a constant scalar.')
 
     @staticmethod
     def graph_implementation(arg_objs,size,data=None):
         x = arg_objs[0]
-        β,x0 = data[0],data[1]
-        β_val,x0_val = β.value,x0.value
+        beta,x0 = data[0],data[1]
+        beta_val,x0_val = beta.value,x0.value
 
-        if isinstance(β,Parameter):
-            β = lu.create_param(β,(1,1))
+        if isinstance(beta,Parameter):
+            beta = lu.create_param(beta,(1,1))
         else:
-            β = lu.create_const(β.value,(1,1))
+            beta = lu.create_const(beta.value,(1,1))
         if isinstance(x0,Parameter):
             x0 = lu.create_param(x0,(1,1))
         else:
             x0 = lu.create_const(x0.value,(1,1))
 
-        ξ,ψ = lu.create_var(size),lu.create_var(size)
+        xi,psi = lu.create_var(size),lu.create_var(size)
         one = lu.create_const(1,(1,1))
-        one_over_β = lu.create_const(1/β_val,(1,1))
-        k = np.exp(-β_val*x0_val)
+        one_over_beta = lu.create_const(1/beta_val,(1,1))
+        k = np.exp(-beta_val*x0_val)
         k = lu.create_const(k,(1,1))
 
-        # 1/β * (1 - exp(-β*(ξ+x0)))
-        ξ_plus_x0 = lu.sum_expr([ξ,x0])
-        minus_β_times_ξ_plus_x0  = lu.neg_expr(lu.mul_expr(β,ξ_plus_x0,size))
-        exp_ξ,constr_exp = exp.graph_implementation([minus_β_times_ξ_plus_x0],size)
-        minus_exp_minus_etc = lu.neg_expr(exp_ξ)
-        left_branch = lu.mul_expr(one_over_β, lu.sum_expr([one,minus_exp_minus_etc]),size)
+        # 1/beta * (1 - exp(-beta*(xi+x0)))
+        xi_plus_x0 = lu.sum_expr([xi,x0])
+        minus_beta_times_xi_plus_x0  = lu.neg_expr(lu.mul_expr(beta,xi_plus_x0,size))
+        exp_xi,constr_exp = exp.graph_implementation([minus_beta_times_xi_plus_x0],size)
+        minus_exp_minus_etc = lu.neg_expr(exp_xi)
+        left_branch = lu.mul_expr(one_over_beta, lu.sum_expr([one,minus_exp_minus_etc]),size)
 
-        # ψ*exp(-β*r0)
-        right_branch = lu.mul_expr(k,ψ,size)
+        # psi*exp(-beta*r0)
+        right_branch = lu.mul_expr(k,psi,size)
 
         obj = lu.sum_expr([left_branch,right_branch])
 
-        #x-x0 == ξ + ψ, ξ >= 0, ψ <= 0
-        zero = lu.create_const(0,(1,1))
+        #x-x0 == xi + psi, xi >= 0, psi <= 0
+        zero = lu.create_const(0,size)
         constraints = constr_exp
-        constraints.append(lu.create_eq(x,lu.sum_expr([x0,ξ,ψ])))
-        constraints.append(lu.create_geq(ξ,zero))
-        constraints.append(lu.create_leq(ψ,zero))
+        prom_x0 = lu.promote(x0, size)
+        constraints.append(lu.create_eq(x,lu.sum_expr([prom_x0,xi,psi])))
+        constraints.append(lu.create_geq(xi,zero))
+        constraints.append(lu.create_leq(psi,zero))
 
         return (obj, constraints)
